d92bcd67624ddec50a033402afb02a96f409b507
hamasaki <hamasaki0011@me.com>
2023-07-04 09:55:34 +0900

    re-push

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import environ
+#23.7.4 import environ
 import os
 from pathlib import Path
 
@@ -21,14 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # Setting the environ
-env = environ.Env()
+#23.7.4 env = environ.Env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-)--oqy25z!@%!dy+mos(jf^f5s_nv)l%3jm8vkp@0un8)s_^qt'
-#SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-#DEBUG = False
 
 ALLOWED_HOSTS = ['localhost']
 # ALLOWED_HOSTS = ['hamasaki.pythonanywhere.com']
